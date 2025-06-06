6c1301dcb8cae0f280b0bf79caf520667674217d
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-06-06 15:04:44 +0530

    fix: improve argument handling for port and origin URL in CLI

--- caching-proxy/src/cli.ts
+++ caching-proxy/src/cli.ts
@@ -1,23 +1,42 @@
 import {startServer} from "./server";
+import {Caching} from "./cache";
+import * as process from "node:process";
 
+const cache = new Caching();
 const args =process.argv.slice(2);
-if (args.length < 2 || !args) {
-    console.error('Usage: node caching-proxy <port> <origin-url>');
-    process.exit(1);
-}
 const getArgValue = (flag: string): string | undefined => {
     const index = args.indexOf(flag);
     return index !== -1 ? args[index + 1] : undefined;
 }
 
-const port = Number(getArgValue('--port'))
-const originUrl  = new URL(getArgValue('--origin-url')!);
 
-if (!originUrl) {
-    console.error('origin url not found');
+// const clearCache = args.includes('--clear-cache');
+// if (clearCache) {
+//     cache.clearAllCache()
+//     process.exit(0)
+// }
+const port = Number(getArgValue('--port'))
+
+const originArg = getArgValue('--origin');
+
+if (!originArg) {
+    console.error('Missing --origin argument');
+    process.exit(1);
+}
+
+let originUrl: URL;
+try {
+    originUrl = new URL(originArg);
+} catch (e) {
+    console.error('Invalid origin URL:', originArg);
+    process.exit(1);
 }
 if (!port) {
     console.error('port not found');
+    process.exit(1);
 }
 
 //startServer
+startServer(port,originUrl,cache).then(() => {
+    console.log(`Proxy Server started at http://localhost:${port}`);
+})
