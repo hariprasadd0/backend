c0eadc4b5b5aa0114f63db7538527b9e9ef5b027
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-06-06 15:04:54 +0530

    feat: enhance server functionality with caching support for fetched URLs

--- caching-proxy/src/server.ts
+++ caching-proxy/src/server.ts
@@ -1,12 +1,41 @@
 import * as http from "http";
+import {Caching} from "./cache";
 
+const startServer = async (port : number,url:string|URL,cache:Caching) => {
 
-const startServer = async (port : number) => {
+    const server = http.createServer(async(req, res) => {
+        const path = req.url || "/";
+        const targetUrl = new URL(path, url).toString()
+        const cached = cache.getCache(targetUrl)
 
-    const server = http.createServer((req, res) => {})
-    server.listen(port, () => {
+        if (cached) {
+            res.writeHead(200, {
+                'Content-Type': 'application/json',
+                'X-Cache': 'HIT',
+                'Cache-Control': 'max-age=60'
+            });
+            return res.end(JSON.stringify(cached));
+        }
+
+        try {
+        const response = await fetch(targetUrl)
+        const data = await response.json()
+        cache.setCache(targetUrl, data);
+            res.writeHead(200, {
+                'Content-Type': 'application/json',
+                'X-Cache': 'MISS',
+                'Cache-Control': 'max-age=60'
+            });
+            return res.end(JSON.stringify(data));
+        }catch (e) {
+            res.writeHead(502, { 'Content-Type': 'text/plain' });
+            return res.end('Failed to fetch target URL');
+        }
+    })
+    server.listen(port,()=>{
         console.log(`Server listening on port ${port}`);
     })
+
 }
 
 export { startServer };
