9f6dd4eda1ed5b1381f6ace48379d8760661b3e3
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-06-11 21:54:00 +0530

    feat : add jwt middleware

---
+++ ABAC/src/middleware/verifyJwt.ts
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import { Request, Response,NextFunction } from 'express';
+
+export const verifyJwt = (req: Request, res: Response, next: NextFunction) => {
+    try{
+        const token = req.header('Authorization')?.replace('Bearer ', '');
+        if (!token) {
+            return res.status(401).send('No token provided');
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+        if (!decoded) {
+            return res.status(401).send('Invalid token');
+        }
+        req.user = decoded;
+        next();
+    }catch(err){
+        res.status(400).send({'something went wrong': 'Invalid token'});
+    }
+}
