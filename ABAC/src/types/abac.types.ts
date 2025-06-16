a9e687707008e646d771efaa3ae980f5bac24d73
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-06-16 16:22:23 +0530

    add AbacTypes and PolicyRule interfaces for attribute-based access control

---
+++ ABAC/src/types/abac.types.ts
@@ -0,0 +1,11 @@
+
+export interface AbacTypes {
+    subject : string;
+    resource : string;
+    action : string;
+}
+
+export interface PolicyRule {
+    policy: string;
+    description?: string;
+}
