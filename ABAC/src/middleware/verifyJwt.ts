5d88fe4f4e5731b998db042167104ed437a64410
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-06-16 16:21:35 +0530

    refactor verifyJwt middleware to use generated access token and improve error handling

--- ABAC/src/middleware/verifyJwt.ts
+++ ABAC/src/middleware/verifyJwt.ts
@@ -1,19 +1,31 @@
 import jwt from 'jsonwebtoken';
-import { Request, Response,NextFunction } from 'express';
+import {NextFunction, Request, Response} from 'express';
+import {User} from "../types/role.ts";
+import {generateAccessToken} from "../utils/token.ts";
 
 export const verifyJwt = (req: Request, res: Response, next: NextFunction) => {
     try{
-        const token = req.header('Authorization')?.replace('Bearer','');
-        if (!token) {
-            return res.status(401).send('No token provided');
+        const user:User = {
+            id: "56f60a68-1f3b-4fa2-9855-4e67f62e3ad6",
+            username: "mock@user.com",
+            email: "mock@user.com",
+            role: "user",
         }
-        const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+        const token = generateAccessToken(user)
+        if (!token) {
+            res.status(401).send('No token provided');
+            return;
+        }
+        const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET!) as User;
+        console.log(decoded);
         if (!decoded) {
-            return res.status(401).send('Invalid token');
+            res.status(401).send('Invalid token');
+            return;
         }
         req.user = decoded;
         next();
     }catch(err){
+        console.log(err)
         res.status(400).send({'something went wrong': 'Invalid token'});
     }
 }
